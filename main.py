625bb742bef587b7c8646b369a0a5bea2615c7b4
Tony <anthony_ventura@student.uml.edu>
2016-11-19 16:48:02 -0500

    Added Costs
    Added costs database in zip. File too large for GitHub so it's zipped
    and will be ignored. Also added a prototype mechanism of retrieving
    flight cost from source airport to destination airport using database

--- main.py
+++ main.py
@@ -7,24 +7,22 @@ class Learning(object):
         super(GraphNode, self).__init__()
         self.flights = arg
         self.airport = None
-        self.discount = discount  
-        
+        self.discount = discount
+
     def computeQValueFromValues(self, currentState, action):
         pass
-        
+
     def computeBestActionFromValues(self, state):
         pass
-    
+
     def getPolicy(self, state):
         return self.computeBestActionFromValues(state)
-
 
     def getQValue(self, state, action):
         #print "zzz", self.computeQValueFromValues(state, action)
         return self.computeQValueFromValues(state, action)
 
-        
-        
+
 class RouteMap(object):
     """docstring for GraphNode"""
     def __init__(self, source, destination):
@@ -33,20 +31,20 @@ class RouteMap(object):
         self.source = source
         self.destination = destination
         self.flights = []
-        
+
     def create(self, dataset): # create map of flights on small dataset
         flyingTo = []
         for row in dataset:
             if row[0] == self.source:
                 if row[1] not in flyingTo:
                     flyingTo.append(row[1])
-            
+
         for row in dataset:
             if row[0] in flyingTo and row[1] == self.destination:
                 if row[0] not in self.connected:
                     self.connected.append(row[0])
         #print self.connected
-    
+
     def train(self, dataset): # add flights for this map on big dataset
         for row in dataset:
             source = row[0]
@@ -57,33 +55,32 @@ class RouteMap(object):
                 or (source in self.connected and destination == self.destination)\
                     or (source == self.source and destination == self.destination):
                 flight = Flight()
-                flight.airport1 = source
-                flight.airport2 = destination
+                flight.source = source
+                flight.destination = destination
                 flight.delayed = delayed
-                
+
                 self.flights.append(flight)
-                
-    
+
+
     def get_routes(self):
         return self.flights
-    
+
     def get_routes(self):
         return self.connected
-        
+
 class Flight(object):
     def __init__(self, arg = None):
         super(Flight, self).__init__()
         self.arg = arg
-        self.airport1 = None
-        self.airport2 = None
+        self.source = None
+        self.destination = None
         self.distance = 0
         self.delayed = None
         self.cancelled = None
-        
+
     def get_flight(self):
         pass
-        
-        
+
     def new_flight(self, source, destination, delayed):
         pass
 
@@ -99,24 +96,40 @@ def load_data(filepath):
             cancelled = row[4]
             dataset.append((source, destination, delay, cancelled))
     return dataset
-            
+
 def qLearning(flightRoutes):
     pass
+
+def estimateCost(flight):
+    src = flight.source
+    dest = flight.destination
+    cost = 0.0
+    count = 0
+
+    with open('costs.csv', 'rb') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[0] == src and row[1] == dest:
+                cost += float(row[2])
+                count += 1
+
+    avg = cost/count
+    return avg
 
 def find(source, destination):
     #parser = argparse.ArgumentParser()
     # parser.add_argument('-s', '--source')
     # parser.add_argument('-d', '--destination')
     # args = parser.parse_args()
-    
-    
+
+
     model = RouteMap(source, destination)
     model_data = load_data('dataset.csv')
     model.create(model_data)
-    
+
     #real_data = load_data('dataset.csv')
     model.train(model_data)
-    
+
     route = {
             "source": model.source,
             "destination": model.destination,
