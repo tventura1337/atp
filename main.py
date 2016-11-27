eb2b49724b8dfcc9c5bf292dd413c55446e7890f
Tony <anthony_ventura@student.uml.edu>
2016-11-27 13:45:10 -0500

    Learning Agent - NEW DEPENDENCIES
    New Dependencies: pandas
    
    Finished building Learning agent. Learning agent takes RouteMap model
    and performs value iteration using data extracted from the model.
    Learning agent takes optional parameters of time sensitivity and number
    of iterations.
    
    Estimate cost function has been optimized to uses pandas to quickly
    extract cost data from costs.csv

--- main.py
+++ main.py
@@ -1,45 +1,84 @@
 import csv
+import pandas as pd
 import argparse
+from collections import Counter
 
 class Learning(object):
     """docstring for GraphNode"""
-    def __init__(self, arg, alpha=0.5, epsilon=0.5):
-        super(GraphNode, self).__init__()
-        self.flights = arg
-        self.airport = None
-        self.discount = discount
-        self.alpha = float(alpha)
-        self.epsilon = float(epsilon)
-        
-        
+    def __init__(self, model, time = 0, iterations = 100):
+        if time:
+            self.discount = 0.8
+        else:
+            self.discount = 1
+        self.model = model
+        self.iterations = iterations
+        self.values = Counter()
 
-    def update(self, state, action, nextState, reward):
-        oldQValue = self.getQValue(state, action)
-        nextQValue = self.getValue(nextState)
-        sample = reward + (self.discount * nextQValue)
+        allStates = self.model.get_all_states()
+        for i in range(iterations):
+            newValues = Counter()
+            for state in allStates:
+                allActions = self.model.get_possible_actions(state)
+                bestValue = float("inf")
+                for action in allActions:
+                    sumTransitionStates = 0
+                    transitionStates = mdp(self.model, state, action)
+                    for nextState, prob in transitionStates:
+                        if len(nextState) > 3:
+                            sumTransitionStates += prob * (1000 + (self.discount * self.getValue(action)))
+                        else:
+                            sumTransitionStates += prob * (self.model.get_cost(state, nextState) + (self.discount * self.getValue(nextState)))
+                    if sumTransitionStates is min(sumTransitionStates, bestValue):
+                        bestValue = sumTransitionStates
+                        newValues[state] = bestValue
+            self.values = newValues
 
-        self.qValues[state, action] = (1 - self.alpha) * oldQValue + self.alpha * sample
+    def computeQValueFromValues(self, state, action):
+        q = 0
+        transitionStates = mdp(self.model, state, action)
 
-    def computeValueFromQValues(self, state):
-        # compute max action q Value
-        pass
+        for nextState, prob in transitionStates:
+            if len(nextState) > 3:
+                q += prob * (1000 + (self.discount * self.getValue(action)))
+            else:
+                q += prob * (self.model.get_cost(state, nextState) + (self.discount * self.getValue(nextState)))
+        return q
 
-    def computeActionFromQValues(self, state):
-        # compute best Action in state
-        pass
+    def computeActionFromValues(self, state):
+        bestValue = float("inf")
+        bestAction = None
+        possibleActions = self.model.get_possible_actions(state)
 
-    def getAction(self, state):
-        # some way to determine where a plane can travel
-        pass
+        for action in possibleActions:
+            if not possibleActions:
+                return None
 
-    def getPolicy(self, state):
-        return self.computeActionFromQValues(state)
+            q = self.getQValue(state, action)
+
+            print "Action is: " + action
+            print "Value is: " + str(q)
+
+            if q is min(q, bestValue):
+
+                bestValue = q
+                bestAction = action
+
+        return bestAction
 
     def getValue(self, state):
-        return self.computeValueFromQValues
+        if state == "cancelled" or state == "delayed":
+            return -10
+        else:
+            return self.values[state]
+
+    def getPolicy(self, state):
+        return self.computeActionFromValues(state)
+
+    def getAction(self, state):
+        return self.computeActionFromValues(state)
 
     def getQValue(self, state, action):
-        return self.qValues[state, action]
+        return self.computeQValueFromValues(state, action)
 
 
 class RouteMap(object):
@@ -50,11 +89,11 @@ class RouteMap(object):
         self.source = source
         self.destination = destination
         self.flights = []
-        
+        self.costs = {}
 
     def create(self, dataset): # create map of flights on small dataset
         flyingTo = []
-        
+
         for row in dataset:
             if row[1] == self.source:
                 if row[2] not in flyingTo:
@@ -65,12 +104,31 @@ class RouteMap(object):
                 if row[1] not in self.connected:
                     self.connected.append(row[1])
 
+    def estimate_costs(self):
+        costData = pd.read_csv('costs.csv', index_col = False)
+        for source in self.get_all_states():
+            src = costData['ORIGIN'] == source
+            allDestinations = self.get_possible_actions(source)
+            for destination in allDestinations:
+                cost = 0.0
+                count = 0
+                dst = costData['DEST'] == destination
+                tot = costData[src & dst]
+                count = len(tot.index)
+                cost = tot['MARKET_FARE'].sum()
+                if cost == count == 0:
+                    avg = 0
+                else:
+                    avg = cost/count
+                self.costs[(source, destination)] = avg
+            #    print source + " to " + destination + " is " + str(self.costs[(source, destination)])
+
     def train(self, dataset): # add flights for this map on big dataset
         for row in dataset:
             source = row[1]
             destination = row[2]
             delayed = row[4]
-            
+
             if row[3] != "":
                 cancelled = int(float(row[3]))
             if row[4] != "":
@@ -81,6 +139,7 @@ class RouteMap(object):
                     or (source == self.source and destination == self.destination):
                 flight = Flight(source, destination, cancelled, delayed)
                 self.flights.append(flight) ##save into flight class
+        self.estimate_costs()
 
     def get_all_airports(self, dataset):
         allAirports = []
@@ -89,22 +148,46 @@ class RouteMap(object):
             code = row[0]
             if (name, code) not in allAirports:
                 allAirports.append((name, code))
-        
+
         return allAirports
-        
+
+    def get_all_states(self):
+        temp = list(self.connected)
+        temp.append(self.source)
+        if self.destination not in temp:
+            temp.append(self.destination)
+        return temp
+
+    def get_possible_actions(self, airport):
+        if airport == self.source:
+            temp = list(self.connected)
+            if self.destination not in temp:
+                temp.append(self.destination)
+            return temp
+        elif airport == self.destination:
+            return []
+        else:
+            temp = self.destination
+            return [temp]
+
     def get_flights(self):
         return self.flights
 
     def get_routes(self):
         return self.connected
-    
+
+    def get_all_costs(self):
+        return self.costs
+
+    def get_cost(self, source, destination):
+        return self.costs[(source, destination)]
+
     def get_specific_flights(self, airport1, airport2):
         specificFlight = []
         for flight in self.flights:
             if flight.find_a_flight(airport1, airport2):
                 specificFlight.append(flight)
         return specificFlight
-            
 
 class Flight(object):
     flightCount = 0
@@ -115,7 +198,8 @@ class Flight(object):
         self.distance = 0
         self.delayed = delayed
         self.cancelled = cancelled
-        
+        self.costs = {}
+
         Flight.flightCount += 1
 
     def find_a_flight(self, airport1, airport2):
@@ -125,7 +209,7 @@ class Flight(object):
 
 def load_data(filepath):
     dataset = []
-    
+
     with open(filepath, 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
@@ -140,9 +224,8 @@ def load_data(filepath):
 
 def mdp(model, source, destination):
     ##example for delayed and cancelled
-    flights = model.get_specific_flights("BOS", "MIA")
+    flights = model.get_specific_flights(source, destination)
     flightsNum, delayedNum, cancelledNum = 0, 0, 0
-
 
     for flight in flights:
         flightsNum += 1
@@ -150,36 +233,31 @@ def mdp(model, source, destination):
             delayedNum +=1
         if flight.cancelled:
             cancelledNum += 1
+
+    if flightsNum == 0:
+        flightsNum += 1
     delayedProb = delayedNum/float(flightsNum)
     cancelledProb = cancelledNum/float(flightsNum)
-    print delayedProb, cancelledProb
-    pass
-    
+    noProb = 1 - (delayedProb + cancelledProb)
 
-def estimateCost(flight):
-    src = flight.airport1
-    dest = flight.airport2
-    cost = 0.0
-    count = 0
-
-    with open('costs.csv', 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if row[0] == src and row[1] == dest:
-                cost += float(row[2])
-                count += 1
-
-    avg = cost/count
-    return avg
+    return [(destination, noProb), ("cancelled", cancelledProb), ("delayed", delayedProb)]
 
 def find(source, destination):
-    
     model = RouteMap(source, destination)
     model_data = load_data('dataset.csv')
     model.create(model_data)
 
     #real_data = load_data('dataset.csv')
+    print "Training Model"
     model.train(model_data)
+    print "Model is trained"
+
+    print "Running value iteration"
+    learn = Learning(model)
+    print "Finished learning"
+
+    action = learn.getAction(source)
+    print "Best airport is:" + action
 
     # route = {
     #         "source": model.source,
@@ -187,7 +265,6 @@ def find(source, destination):
     #         "routes": model.get_specific_flights("BOS", "MIA")[0].flightCount
     #         }
     # rt = model.get_specific_flights("BOS", "MIA")
-    mdp(model, source, destination)
     # return route
 
 
@@ -202,9 +279,11 @@ def airports_list():
             }
     return json
 
+def computeCosts(source, destination):
+    pass
 
 def main():
     find("JFK", "MIA")
-    
+
 if __name__ == "__main__":
     main()
